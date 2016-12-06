3ded2f6be75fead998b7e74762df421edd33b6d2
palegret <philip.alegret@gmail.com>
2016-12-06 15:31:54 -0500

    Final round of reactive form changes.

--- app/reactive-form/reactive-form.component.ts
+++ app/reactive-form/reactive-form.component.ts
@@ -11,55 +11,60 @@ export class ReactiveFormComponent implements OnInit {
 	user: User
 	form: FormGroup
   submitted: boolean = false
-	nameError: string
-	usernameError: string
-	
-	constructor(private formBuilder: FormBuilder) {	
+
+	formErrors: any = {
+		name: '',
+		username: ''
 	}
+
+	validationMessages: any = {
+		name: {
+			required: 'Name is required.',
+			minlength: 'Name must be at least 3 characters.',
+			maxlength: 'Name cannot be more than 5 characters.'
+		},
+		username: {
+			required: 'Username is required.',
+			minlength: 'Username must be at least 3 characters.'
+		}		
+	}	
+
+	constructor(private formBuilder: FormBuilder) {}
 
   get diagnostic() {
     return JSON.stringify(this.form.value);
   }
 
+	private formConfig: any = {
+		name: ['', [
+			Validators.minLength(3), 
+			Validators.maxLength(6)
+		]],
+		username: ['', [
+			Validators.minLength(3)
+		]]
+	};
+
+	private validateForm(): void {
+		for (let field in this.formErrors) {
+			this.formErrors[field] = '';
+
+			let input = this.form.get(field);
+
+			if (this.invalid(input))
+				for (let error in input.errors)
+					this.formErrors[field] = this.validationMessages[field][error];
+		}
+	}
+
+	private buildForm(): void {
+		this.form = this.formBuilder.group(this.formConfig);
+		this.form.valueChanges.subscribe(formData => this.validateForm());
+	}
+
   ngOnInit() {
     this.user = new User(); 
-
-		this.form = this.formBuilder.group({
-			name: ['', [
-				Validators.minLength(3), 
-				Validators.maxLength(6)
-			]],
-			username: ['', [
-				Validators.minLength(3)
-			]]
-		});
-
-		this.form.valueChanges.subscribe(formData => {
-			this.nameError = null;
-			this.usernameError = null;
-
-			let name = this.form.get('name');
-			let username = this.form.get('username');
-
-			if (this.invalid(name)) {
-				if (name.errors['required'])
-					this.nameError = 'Name is required.';
-
-				if (name.errors['minlength'])
-					this.nameError = 'Name must be at least 3 characters.';
-
-				if (name.errors['maxlength'])
-					this.nameError = 'Name cannot be more than 5 characters.';
-			}
-
-			if (this.invalid(username)) {
-				if (username.errors['required'])
-					this.usernameError = 'Username is required.';
-
-				if (username.errors['minlength'])
-					this.usernameError = 'Username must be at least 3 characters.';				
-			}
-		});
+		this.buildForm();
   }
 
   valid(model: any): boolean {
