3433175cad77a1a644dab66e15187fb0b82b9105
palegret <philip.alegret@gmail.com>
2016-12-08 03:49:26 -0500

    Finished reactive form, added Font Awesome.

--- app/reactive-form/reactive-form.component.ts
+++ app/reactive-form/reactive-form.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { User } from '../shared/models/user';
+import { AbstractControl, FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'reactive-form',
@@ -8,16 +7,18 @@ import { User } from '../shared/models/user';
   styleUrls: ['./app/reactive-form/reactive-form.component.css']
 })
 export class ReactiveFormComponent implements OnInit {
-	user: User
-	form: FormGroup
-  submitted: boolean = false
+	public form: FormGroup
+  public submitted: boolean = false
 
-	formErrors: any = {
+	public formErrors: any = {
 		name: '',
-		username: ''
+		username: '',
+		addresses: [
+			{ city: '', country: '' }
+		]
 	}
 
-	validationMessages: any = {
+	public validationMessages: any = {
 		name: {
 			required: 'Name is required.',
 			minlength: 'Name must be at least 3 characters.',
@@ -26,24 +27,69 @@ export class ReactiveFormComponent implements OnInit {
 		username: {
 			required: 'Username is required.',
 			minlength: 'Username must be at least 3 characters.'
-		}		
+		},		
+		addresses: {
+			city: {
+				required: 'City is required.',
+				minlength: 'City must be at least 3 characters.'
+			},
+			country: {
+				required: 'Country is required.',
+				minlength: 'Country must be at least 3 characters.'
+			}			
+		}
 	}	
 
-	constructor(private formBuilder: FormBuilder) {}
-
-  get diagnostic() {
+  public get diagnostic(): string {
     return JSON.stringify(this.form.value);
   }
 
+	constructor(private formBuilder: FormBuilder) {}
+
 	private formConfig: any = {
 		name: ['', [
+			Validators.required,
 			Validators.minLength(3), 
 			Validators.maxLength(6)
 		]],
 		username: ['', [
+			Validators.required,
 			Validators.minLength(3)
-		]]
+		]],
+		addresses: this.formBuilder.array([
+			this.formBuilder.group({
+				city: ['', [
+					Validators.required,
+					Validators.minLength(3)
+				]],
+				country: ['', [
+					Validators.required
+				]]
+			})
+		])
 	};
+	
+  private validateAddresses(): void {
+    this.formErrors.addresses = [];
+
+    let addresses = <FormArray>this.form.get('addresses');
+
+    for (let n = 0, len = addresses.length; n < len; n++) {
+			// Push new validation error object for address n
+      this.formErrors.addresses.push({ city: '', country: '' });
+
+			// Get specific FormGroup for address n
+      let address = <FormGroup>addresses.at(n);
+
+      for (let field in address.controls) {
+        let input = address.get(field);
+
+				if (this.invalid(input))
+          for (let error in input.errors)
+            this.formErrors.addresses[n][field] = this.validationMessages.addresses[field][error];
+      }
+    }
+  }
 
 	private validateForm(): void {
 		for (let field in this.formErrors) {
@@ -55,6 +101,8 @@ export class ReactiveFormComponent implements OnInit {
 				for (let error in input.errors)
 					this.formErrors[field] = this.validationMessages[field][error];
 		}
+
+		this.validateAddresses();
 	}
 
 	private buildForm(): void {
@@ -62,29 +110,65 @@ export class ReactiveFormComponent implements OnInit {
 		this.form.valueChanges.subscribe(formData => this.validateForm());
 	}
 
-  ngOnInit() {
-    this.user = new User(); 
+	private createAddress(): FormGroup {
+		return this.formBuilder.group({
+			city: ['', [
+				Validators.required,
+				Validators.minLength(3)
+			]],
+			country: ['', [
+				Validators.required
+			]]
+		});
+	}
+
+  public ngOnInit(): void {
 		this.buildForm();
   }
 
-  valid(model: any): boolean {
-    return model && model.dirty && model.valid;
+  public valid(control: AbstractControl): boolean {
+    return control && control.dirty && control.valid;
   }
   
-  invalid(model: any): boolean {
-    return model && model.dirty && model.invalid;
+  public invalid(control: AbstractControl): boolean {
+    return control && control.dirty && control.invalid;
   }
-  
-  validationClass(model: any): string {
-  	if (this.valid(model))
+
+  public validationClass(control: AbstractControl): string {
+  	if (this.valid(control))
       return 'has-success';      
-  	else if (this.invalid(model))
+  	else if (this.invalid(control))
       return 'has-error';
     else
       return '';
+  }	
+
+	// Alternate validation CSS method, not sure I like it though.
+	// FormControl: ...[ngClass]="validationClass('username')...
+	// FormArray: ...[ngClass]="validationClass('addresses', i, 'country')...	
+  public validationClass2(formControlName: string, index?: number, propertyName?: string): string {
+		let control: AbstractControl = this.form.get(formControlName);
+		let className: string = control.constructor.name;
+
+		if (className === 'FormArray') {
+			let element = <FormGroup>(<FormArray>control).at(index);
+			control = element.get(propertyName);
+		}
+
+		return this.validationClass(control);
   }
 
-  processForm() {
+  public processForm(): void {
     this.submitted  = true;
   }
+
+	public addAddress(): void {
+		let addresses = <FormArray>this.form.get('addresses');
+		addresses.push(this.createAddress());
+	}	
+	
+	public removeAddress(index: number): void {
+		let addresses = <FormArray>this.form.get('addresses');
+		addresses.removeAt(index);
+	}
 }
